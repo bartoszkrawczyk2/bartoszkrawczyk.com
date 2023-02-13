3b4cd66bf6048bfc1471ba03522767d1c612cad2
Bartosz Krawczyk <bartoszkrawczyk2@gmail.com>
2023-02-13 10:34:00 +0100

    Add analitics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Inter } from '@next/font/google';
 import classNames from 'classnames';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <div className={classNames(inter.variable, 'font-sans')}>
         <Component {...pageProps} />
+        <Analytics />
       </div>
     </>
   );
